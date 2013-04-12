506e31fc4fc1cb5b47ef5a9f8ef11c2d29f9e79b
Holger Rother <hrother@hrother.org>
2013-04-12 20:02:31 +0200

    oh-my-zsh: Host specific directory shortcuts

--- oh-my-zsh/custom/plugins/dir_abriv/dir_abriv.plugin.zsh
+++ oh-my-zsh/custom/plugins/dir_abriv/dir_abriv.plugin.zsh
@@ -1,6 +1,9 @@
 # Add yourself some shortcuts to projects you often work on
 
-# Bsc-thesis
-alias b='cd ~/bsc-thesis/'
-alias bt='cd ~/bsc-thesis/thesis/'
-alias bs='cd ~/bsc-thesis/slingshot/'
+# Host specific shortcuts
+if [[ `hostname` == 'magrathea' ]]; then
+  # Bsc-thesis
+  alias b='cd ~/bsc-thesis/'
+  alias bt='cd ~/bsc-thesis/thesis/'
+  alias bs='cd ~/bsc-thesis/slingshot/'
+fi
