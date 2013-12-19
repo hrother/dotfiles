063ceabefef288bd9c1519d92b21171b50d27c75
Holger Rother <hrother@hrother.org>
2013-12-19 14:23:34 +0100

    pythonbrew support

--- oh-my-zsh/custom/plugins/env/env.plugin.zsh
+++ oh-my-zsh/custom/plugins/env/env.plugin.zsh
@@ -15,14 +15,16 @@ fi
 # Disable Terminal Flow Control
 stty -ixon
 
+# disabled due to use of pythonbrew
 # setup python virtualenvwrapper
-if [ `id -u` != '0' ]; then
-  export VIRTUALENV_USE_DISTRIBUTE=1        # <-- Always use pip/distribute
-  export WORKON_HOME=$HOME/.virtualenvs       # <-- Where all virtualenvs will be stored
-  source /usr/local/bin/virtualenvwrapper.sh
-  export PIP_VIRTUALENV_BASE=$WORKON_HOME
-  export PIP_RESPECT_VIRTUALENV=true
-fi
+#if [ `id -u` != '0' ]; then
+  #export VIRTUALENV_USE_DISTRIBUTE=1        # <-- Always use pip/distribute
+  #export WORKON_HOME=$HOME/.virtualenvs       # <-- Where all virtualenvs will be stored
+  #source /usr/local/bin/virtualenvwrapper.sh
+  #export PIP_VIRTUALENV_BASE=$WORKON_HOME
+  #export PIP_RESPECT_VIRTUALENV=true
+#fi
 
 export SHELL="/usr/bin/zsh"
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM functio
+[[ -s "$HOME/.pythonbrew/etc/bashrc" ]] && . "$HOME/.pythonbrew/etc/bashrc" # load pythonbrew
