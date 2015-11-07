a87807e3d00373f370497afefe91c621f94f8467
Holger Rother <hrother@hrother.org>
2015-11-07 19:25:04 +0100

    Cleanup environment variables and adjust miniconda

--- oh-my-zsh/custom/plugins/env/env.plugin.zsh
+++ oh-my-zsh/custom/plugins/env/env.plugin.zsh
@@ -15,18 +15,6 @@ fi
 # Disable Terminal Flow Control
 stty -ixon
 
-# disabled due to use of pythonbrew
-# setup python virtualenvwrapper
-#if [ `id -u` != '0' ]; then
-  #export VIRTUALENV_USE_DISTRIBUTE=1        # <-- Always use pip/distribute
-  #export WORKON_HOME=$HOME/.virtualenvs       # <-- Where all virtualenvs will be stored
-  #source /usr/local/bin/virtualenvwrapper.sh
-  #export PIP_VIRTUALENV_BASE=$WORKON_HOME
-  #export PIP_RESPECT_VIRTUALENV=true
-#fi
 export TERM=screen-256color
 export SHELL="/usr/bin/zsh"
-[[ -s "$HOME/.pythonbrew/etc/bashrc" ]] && . "$HOME/.pythonbrew/etc/bashrc" # load pythonbrew
-export GRADLE_HOME=/usr/local/gradle-2.2.1
-export PATH=$PATH:$GRADLE_HOME/bin
-export PATH="/home/holger/miniconda3/bin:$PATH"
+export PATH="/home/holger/.miniconda3/bin:$PATH"
